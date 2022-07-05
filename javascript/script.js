9bc9a6f8da201d15b15bbd1aafc66234058acae2
Hannah Steil <h-steil@t-online.de>
2022-07-05 19:22:55 +0200

    Change styles of the answer

--- javascript/script.js
+++ javascript/script.js
@@ -17,7 +17,7 @@ const answerButton = document.querySelector('[data-js="button-answer"]');
 const answer = document.querySelector('[data-js="answer"]');
 
 answerButton.addEventListener("click", () => {
-  answer.classList.toggle("card_answer");
+  answer.classList.toggle("card_answer--hidden");
   if (answerButton.textContent.trim() == "Verberge Antwort") {
     answerButton.textContent = "Zeige Antwort";
   } else if (answerButton.textContent.trim() == "Zeige Antwort") {
