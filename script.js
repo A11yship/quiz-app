4b101ae57e81e27637bc461fa513d3ecae3f0057
Hannah Steil <h-steil@t-online.de>
2022-07-07 21:04:12 +0200

    Create directory for card component and move files there

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 import Bookmark from "./components/bookmark/bookmark.js";
+import Card from "./components/card/card.js";
 console.clear();
 
-//Change the bookmark status
-
 Bookmark();
-
-//Hidde/show answer
-
-const answerButton = document.querySelector('[data-js="button-answer"]');
-const answer = document.querySelector('[data-js="answer"]');
-
-answerButton.addEventListener("click", () => {
-  answer.classList.toggle("card_answer--hidden");
-  if (answerButton.textContent.trim() == "Verberge Antwort") {
-    answerButton.textContent = "Zeige Antwort";
-  } else if (answerButton.textContent.trim() == "Zeige Antwort") {
-    answerButton.textContent = "Verberge Antwort";
-  }
-});
-
-console.log([answerButton]);
+Card();
