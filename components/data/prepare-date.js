4af667186c2924eb71d0da9e0c8ebd6700798429
Hannah Steil <h-steil@t-online.de>
2022-07-18 21:31:23 +0200

    Add function to decode HTML entities

--- components/data/prepare-date.js
+++ components/data/prepare-date.js
@@ -1,8 +1,10 @@
+import DecodeEntities from "./decodeEntities.js";
+
 export default function PrepareDate(dataArray) {
   return dataArray.map((item) => {
     const answerOptions = [];
     if (item.type == "multiple") {
-      answerOptions.push(item.correct_answer);
+      answerOptions.push(DecodeEntities(item.correct_answer));
       item.incorrect_answers.forEach((answer) => answerOptions.push(answer));
     }
 
@@ -16,8 +18,8 @@ export default function PrepareDate(dataArray) {
     }
 
     return {
-      question: item.question,
-      answer: item.correct_answer,
+      question: DecodeEntities(item.question),
+      answer: DecodeEntities(item.correct_answer),
       answer_options: answerOptions.sort(),
       tags: tagList,
     };
